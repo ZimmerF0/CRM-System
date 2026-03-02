d16dbb78007d223f3d9bb4df162e05942f39a628
Kishkin Vitalii <dimpla30@gmail.com>
2026-03-02 11:40:44 +0300

    add .env file

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 export const axiosClient = axios.create({
-  baseURL: "https://easydev.club/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
