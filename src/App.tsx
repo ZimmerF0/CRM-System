bbb0dd928ab4f207bcae5b6c65d739d4eb9a8642
Kishkin Vitalii <dimpla30@gmail.com>
2026-03-29 19:55:10 +0300

    finish fix after review

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
-import { Spin } from "antd";
+
 import AuthLayout from "./layouts/AuthLayout/AuthLayout";
 import MainLayout from "./layouts/MainLayout/MainLayout";
 import TodoListPage from "./pages/TodoList/TodoListPage";
@@ -8,27 +8,15 @@ import ProfilePage from "./pages/Profile/ProfilePage";
 import LoginPage from "./pages/Login/LoginPage";
 import RegisterPage from "./pages/Register/RegisterPage";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { useAppDispatch, useAppSelector } from "./store/hooks.ts";
+import { useAppDispatch } from "./store/hooks.ts";
 import { refresh } from "./store/auth/thunks";
 
 export default function App() {
   const dispatch = useAppDispatch();
 
-  const isAuthChecked = useAppSelector(state => state.auth.isAuthChecked);
-
-  const didRefresh = useRef(false);
-
   useEffect(() => {
-    if (!didRefresh.current) {
-      //refresh вызовется один раз
-      dispatch(refresh());
-      didRefresh.current = true;
-    }
+    dispatch(refresh());
   }, [dispatch]);
-
-  if (!isAuthChecked) {
-    return <Spin fullscreen />;
-  }
 
   return (
     <BrowserRouter>
--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,16 @@
 import { Navigate } from "react-router";
 import { useAppSelector } from "../store/hooks";
 import type { PropsWithChildren } from "react";
+import { Spin } from "antd";
 
 export default function ProtectedRoute({ children }: PropsWithChildren) {
-  const isAuthenticated = useAppSelector(
-    (state) => state.auth.isAuthenticated
+  const {isAuthenticated, isAuthChecked} = useAppSelector(
+    (state) => state.auth
   );
 
+  if (!isAuthChecked) {
+    return <Spin fullscreen />;
+  }
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
